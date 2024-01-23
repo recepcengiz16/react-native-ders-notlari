d208921ff01bb8cb8d7c87bdb196101e6a5bbe39
Recep <recepcengiz_96@outlook.com>
2024-01-23 17:29:00 +0300

    box uygulaması oluşturuldu

---
+++ use-state-kullanimi/src/screens/BoxScreen.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function BoxScreen() {
+  return (
+    <View>
+      <Text>BoxScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
