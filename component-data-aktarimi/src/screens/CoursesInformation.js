49cb23af5f6e1a642524cbe71078fc4ac7fa0eb8
Recep <recepcengiz_96@outlook.com>
2024-01-23 14:03:16 +0300

    Kurs bilgilerim sayfasında eğitimler ile ilgili düzenlemeler yapıldı

--- component-data-aktarimi/src/screens/CoursesInformation.js
+++ component-data-aktarimi/src/screens/CoursesInformation.js
@@ -5,7 +5,11 @@ import Information from '../../components/Information'
 export default function CoursesInformation() {
   return (
     <View>
-        <Information></Information>
+        <Information title="Bootstrap Eğitimi" imageSource={require('../../assets/bootstrap.png')} desc="Kapsamlı Bootstrap Eğitimi" /> 
+        {/* resim verirken requireile yapmak lazım */}
+        <Information title="React Eğitimi" imageSource={require('../../assets/react.png')} desc="Kapsamlı React Eğitimi" />
+        <Information title="Vue Eğitimi" imageSource={require('../../assets/vue.jpg')} desc="Kapsamlı Vue Eğitimi" />
+       
     </View>
   )
 }
