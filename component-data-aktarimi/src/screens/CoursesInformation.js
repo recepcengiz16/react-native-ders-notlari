8f1524aa2577483cbbe34beba284eab2a0e0d0e9
Recep <recepcengiz_96@outlook.com>
2024-01-23 15:03:54 +0300

    scrollview kullandım normal view yerine

--- component-data-aktarimi/src/screens/CoursesInformation.js
+++ component-data-aktarimi/src/screens/CoursesInformation.js
@@ -1,16 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Information from '../../components/Information'
 
 export default function CoursesInformation() {
   return (
-    <View>
+    <ScrollView> {/* normal view de ekranı aşağı kaydıramıyorsun. scrollview ile kaydırmana olanak sağlanmış oluyor. */}
         <Information title="Bootstrap Eğitimi" imageSource={require('../../assets/bootstrap.png')} desc="Kapsamlı Bootstrap Eğitimi" /> 
         {/* resim verirken requireile yapmak lazım */}
         <Information title="React Eğitimi" imageSource={require('../../assets/react.png')} desc="Kapsamlı React Eğitimi" />
         <Information title="Vue Eğitimi" imageSource={require('../../assets/vue.jpg')} desc="Kapsamlı Vue Eğitimi" />
        
-    </View>
+    </ScrollView>
   )
 }
 
