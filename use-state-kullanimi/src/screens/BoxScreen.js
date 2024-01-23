4383510cb91348bf214788bcc0d9c1eed13d4afc
Recep <recepcengiz_96@outlook.com>
2024-01-23 17:56:07 +0300

    yorum satırı yazdım

--- use-state-kullanimi/src/screens/BoxScreen.js
+++ use-state-kullanimi/src/screens/BoxScreen.js
@@ -12,7 +12,7 @@ export default function BoxScreen() {
   return (
     <View>
       <Button title='Kutu Ekle' onPress={()=>{
-         setColors([...colors,randomColor()])
+         setColors([...colors,randomColor()]) //burada colors dizisini aynen alıp, daha sonra eklenecek olduğunda randomColor ile yap manasında. setColors([...colors, randomColor()]) ifadesi, mevcut colors dizisinin kopyasını oluşturarak ve bu kopyaya randomColor() fonksiyonunun ürettiği rastgele bir renk ekleyerek yeni bir dizi oluşturur. Sonrasında, bu yeni diziyi setColors fonksiyonu aracılığıyla state'e atar.
       }} />
 
       <FlatList
