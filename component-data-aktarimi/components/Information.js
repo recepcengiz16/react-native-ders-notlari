fe16077b11aeca4d23e4383fc0a9d7b4da023df6
Recep <recepcengiz_96@outlook.com>
2024-01-23 14:06:59 +0300

    props yöntemi ile üst componentten data aktarımı yapıldı

--- component-data-aktarimi/components/Information.js
+++ component-data-aktarimi/components/Information.js
@@ -1,10 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-export default function Information() {
+export default function Information({title,desc,imageSource}) { //normalde (props) yazıp aşağıda props.title diyerek de erişebilirsin ya da js destractive yapısı var ya obje notasyonunda yapıyorsun
   return (
     <View>
-      <Text>Information</Text>
+      <Text>{title}</Text>
     </View>
   )
 }
