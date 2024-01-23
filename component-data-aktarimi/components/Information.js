5f43fa0565540b7f9c54f14209bda246d902e2ac
Recep <recepcengiz_96@outlook.com>
2024-01-23 15:01:28 +0300

    information componenti içeriği oluşturdum

--- component-data-aktarimi/components/Information.js
+++ component-data-aktarimi/components/Information.js
@@ -1,10 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,Image } from 'react-native'
 import React from 'react'
 
 export default function Information({title,desc,imageSource}) { //normalde (props) yazıp aşağıda props.title diyerek de erişebilirsin ya da js destractive yapısı var ya obje notasyonunda yapıyorsun
   return (
     <View>
+      <Image source={imageSource} style={{height:200,width:200}}></Image>
       <Text>{title}</Text>
+      <Text>{desc}</Text>
     </View>
   )
 }
