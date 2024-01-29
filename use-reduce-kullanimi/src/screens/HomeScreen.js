ee6263fd7903ad32c131de8b6f5bf88f464f92f9
Recep <recepcengiz_96@outlook.com>
2024-01-29 11:03:00 +0300

    Ana Sayfa ve gideceği sayfa tasarlandı

---
+++ use-reduce-kullanimi/src/screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View,Button } from 'react-native'
+import React from 'react'
+
+export default function HomeScreen({navigation}) {
+  return (
+    <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
+      <Text> Ana Sayfa </Text>
+      <Button onPress={()=> navigation.navigate("Sayac") } title='Sayfaya Git' />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
