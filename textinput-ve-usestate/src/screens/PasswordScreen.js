b44580cfe6364b36ec14da99c83a79764852a16b
Recep <recepcengiz_96@outlook.com>
2024-01-30 09:02:51 +0300

    Ekrandan textinput ile değer almayı ve o değeri ekrana bastırmayı öğrendik

--- textinput-ve-usestate/src/screens/PasswordScreen.js
+++ textinput-ve-usestate/src/screens/PasswordScreen.js
@@ -1,12 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View,TextInput } from 'react-native'
+import React,{useState} from 'react'
 
 export default function PasswordScreen() {
-  return (
-    <View>
-      <Text>Şifre Sayfası</Text>
+    const [password, setPassword] = useState("");
+  
+    return (
+    <View style={styles.main}>
+      <Text>Şifre Giriniz: </Text>
+      <TextInput 
+        style={styles.input}
+        autoCapitalize='none' 
+        autoCorrect={false}
+        value={password} //onchange ile passwordun yeni değeri elde edilmiş oluyor ve ekrana basarken de value ile yeni değeri alıyoruz
+        onChangeText={(newValue)=>{setPassword(newValue)}}
+      /> 
+      {password.length < 6 ? (<Text>Şifre en az 6 karakter olmalıdır.</Text>) : null}
+      {/* 
+      autoCapitalize='none': textboxa yazarken otomatik büyük harfle başlıyor, otomatik büyük harfle başlamasını kapattım autocapitalize='none' ile
+      autoCorrect={false} => bu da telefonda bir şey yazarken tahmin çıkıyor ya onu kapatıyor
+      
+      */}
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    main:{
+        margin:10
+    },
+    input:{
+        borderColor:"black",
+        borderWidth:1,
+        padding:5
+    }
+})
