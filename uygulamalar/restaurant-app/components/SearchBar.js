0ec09dc38492d501dafb4a6aa386ec63b879a404
Recep <recepcengiz_96@outlook.com>
2024-01-30 17:38:23 +0300

    textinput ve search iconu eklendi

--- uygulamalar/restaurant-app/components/SearchBar.js
+++ uygulamalar/restaurant-app/components/SearchBar.js
@@ -4,11 +4,27 @@ import { AntDesign } from '@expo/vector-icons';
 
 export default function SearchBar() {
   return (
-    <View>
-      <AntDesign name="search1" size={24} color="black" />
-      <TextInput style={{borderColor:"black",borderWidth:1}} />
+    <View style={styles.backGroundStyle}>
+      <AntDesign style={styles.iconStyle} name="search1" size={30} color="black" />
+      <TextInput style={styles.inputStyle} placeholder='Ara' autoCorrect={false} autoCapitalize='none' />
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    backGroundStyle:{
+        backgroundColor:"lightgray",
+        margin:10,
+        height:80,
+        flexDirection:"row",
+        alignItems:"center",
+        borderRadius:40
+    },
+    iconStyle:{
+        marginHorizontal:15
+    },
+    inputStyle:{
+        flex:1,
+        fontSize:18
+    }
+})
