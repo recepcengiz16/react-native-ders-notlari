ae94c754336fdc5a60628f47a8e439662813a5e0
Recep <recepcengiz_96@outlook.com>
2024-01-30 15:51:12 +0300

    textinput eklendi

--- uygulamalar/restaurant-app/components/SearchBar.js
+++ uygulamalar/restaurant-app/components/SearchBar.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,TextInput } from 'react-native'
 import React from 'react'
 import { AntDesign } from '@expo/vector-icons';
 
@@ -6,6 +6,7 @@ export default function SearchBar() {
   return (
     <View>
       <AntDesign name="search1" size={24} color="black" />
+      <TextInput style={{borderColor:"black",borderWidth:1}} />
     </View>
   )
 }
