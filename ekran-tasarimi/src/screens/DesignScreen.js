a2ce304f8c29136929f434dbffd6b1bd7c7cf4fd
Recep <recepcengiz_96@outlook.com>
2024-01-30 10:35:01 +0300

    flex değeri ile hizalama yapıldı

--- ekran-tasarimi/src/screens/DesignScreen.js
+++ ekran-tasarimi/src/screens/DesignScreen.js
@@ -6,7 +6,11 @@ export default function DesignScreen() {
     <View>
        <View style={{width:50,height:50,backgroundColor:"red"}} />
 
-        <Text style={styles.mainText}> Layout </Text>
+        <Text style={styles.mainText}> 1 </Text>
+        
+        <Text style={styles.mainText}> 2 </Text>
+        
+        <Text style={styles.mainText}> 3 </Text>
 
 
     </View>
@@ -15,10 +19,17 @@ export default function DesignScreen() {
 
 const styles = StyleSheet.create({
     mainView:{
+        //flexDirection:"row" webde display flex default olarak row dur ama react nativede default olarak column dur. bundan alignitems ve justifyContent de etkileniyor. Kapsayıcıda hizalama yapıyor  
+        
+        //alignItems normalde webde dikey hizalamadır ama react nativede yatay hizalama yapıyor.
+        //justifyContent react nativede dikey hizalama yapıyor
+        //flexDirection:"row"
+        
         borderWidth:1,
         borderColor:"black",
         margin:20,
-        borderRadius:10
+        borderRadius:10,
+        alignItems:"flex-start", 
     },
     mainText:{
         borderWidth:1,
