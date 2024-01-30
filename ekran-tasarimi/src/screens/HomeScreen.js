a9fbd0b8d786768232b2d1f99a23af3dae9f85a6
Recep <recepcengiz_96@outlook.com>
2024-01-30 09:44:40 +0300

    navigation yönlendirmesini vermeyi unutmuştum

--- ekran-tasarimi/src/screens/HomeScreen.js
+++ ekran-tasarimi/src/screens/HomeScreen.js
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View,Button } from 'react-native'
 import React from 'react'
 
-export default function HomeScreen() {
+export default function HomeScreen({navigation}) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
