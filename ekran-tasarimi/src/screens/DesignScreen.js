d582b06157d51bd938a6411ff465d8312d88ad2b
Recep <recepcengiz_96@outlook.com>
2024-01-30 11:44:29 +0300

    alignSelf css propertysini bakıldı

--- ekran-tasarimi/src/screens/DesignScreen.js
+++ ekran-tasarimi/src/screens/DesignScreen.js
@@ -18,6 +18,9 @@ export default function DesignScreen() {
 }
 
 const styles = StyleSheet.create({
+
+    //child componente kendi kendine hizalama yapmak istediğimizde alignSelf ten yararlanabilirim.
+
     mainView:{
         //flexDirection:"row" webde display flex default olarak row dur ama react nativede default olarak column dur. bundan alignitems ve justifyContent de etkileniyor. Kapsayıcıda hizalama yapıyor  
         
@@ -37,7 +40,15 @@ const styles = StyleSheet.create({
         margin:30,
         padding:10,
         borderRadius:5,
-        flex:4 //verirken bootstrap de 12 ya burada da 10. flex 4 deyince %40 gibi düşün. Bunu hep 10 a tamamlayacak gibi düşün. Dağtımı flex:2, diğer flexlere de 2 ve 1 verirsek %40 %40 %20 olur bu seferde.
+        //position:"absolute" kapsayıcıya göre konumlandırma yapıyor. Hangi kapsayıcısı varsa yani bir üst parentı neyse ona göre hizalıyor
+        // top:0, bu 4 ü de position absolute da her taraftan 0 boşluk verecek bunu kapsayan divin genişliği ve yüksekliği gibi olacak
+        // bottom:0,
+        // left:0,
+        // right:0
+
+
+        //alignSelf:"flex-end" bu da kendi kendine hizalatıyor. yani sadece bu etkileniyor
+        //flex:4 //verirken bootstrap de 12 ya burada da 10. flex 4 deyince %40 gibi düşün. Bunu hep 10 a tamamlayacak gibi düşün. Dağtımı flex:2, diğer flexlere de 2 ve 1 verirsek %40 %40 %20 olur bu seferde.
     },
     mainTextTwo:{
         borderWidth:1,
@@ -45,7 +56,7 @@ const styles = StyleSheet.create({
         margin:30,
         padding:10,
         borderRadius:5,
-        flex:5
+        //flex:5
     },
     mainTextThree:{
         borderWidth:1,
@@ -53,6 +64,6 @@ const styles = StyleSheet.create({
         margin:30,
         padding:10,
         borderRadius:5,
-        flex:1 //mesela yukarıdaki mainlerde flex yazmadık geldik en sonki elementin main cssine yazdık. Üstlerde flex olmayacak ya bundan dolayı kalan boşluğu da buradaki flex kapatacak. Boş alana flex atayınca orayı da flex kapatıyor 
+        //flex:1 //mesela yukarıdaki mainlerde flex yazmadık geldik en sonki elementin main cssine yazdık. Üstlerde flex olmayacak ya bundan dolayı kalan boşluğu da buradaki flex kapatacak. Boş alana flex atayınca orayı da flex kapatıyor 
     }
 })
