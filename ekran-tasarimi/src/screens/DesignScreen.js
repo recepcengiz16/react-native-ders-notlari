4c237462dc13b8d4871a48f1d06f86d0c1ce3df5
Recep <recepcengiz_96@outlook.com>
2024-01-30 10:50:33 +0300

    flex de dikey ve yatay hizalama nasıl olur

--- ekran-tasarimi/src/screens/DesignScreen.js
+++ ekran-tasarimi/src/screens/DesignScreen.js
@@ -3,14 +3,14 @@ import React from 'react'
 
 export default function DesignScreen() {
   return (
-    <View>
-       <View style={{width:50,height:50,backgroundColor:"red"}} />
+    <View style={styles.mainView}>
+        {/* <View style={{width:50,height:50,backgroundColor:"red"}} /> */}
 
-        <Text style={styles.mainText}> 1 </Text>
+        <Text style={styles.mainTextOne}> 1 </Text>
         
-        <Text style={styles.mainText}> 2 </Text>
+        <Text style={styles.mainTextTwo}> 2 </Text>
         
-        <Text style={styles.mainText}> 3 </Text>
+        <Text style={styles.mainTextThree}> 3 </Text>
 
 
     </View>
@@ -23,19 +23,36 @@ const styles = StyleSheet.create({
         
         //alignItems normalde webde dikey hizalamadır ama react nativede yatay hizalama yapıyor.
         //justifyContent react nativede dikey hizalama yapıyor
-        //flexDirection:"row"
         
         borderWidth:1,
         borderColor:"black",
         margin:20,
         borderRadius:10,
-        alignItems:"flex-start", 
+        height:740
+        //alignItems:"flex-start", 
     },
-    mainText:{
+    mainTextOne:{
         borderWidth:1,
         borderColor:"red",
         margin:30,
         padding:10,
-        borderRadius:5
+        borderRadius:5,
+        flex:4 //verirken bootstrap de 12 ya burada da 10. flex 4 deyince %40 gibi düşün. Bunu hep 10 a tamamlayacak gibi düşün. Dağtımı flex:2, diğer flexlere de 2 ve 1 verirsek %40 %40 %20 olur bu seferde.
+    },
+    mainTextTwo:{
+        borderWidth:1,
+        borderColor:"red",
+        margin:30,
+        padding:10,
+        borderRadius:5,
+        flex:5
+    },
+    mainTextThree:{
+        borderWidth:1,
+        borderColor:"red",
+        margin:30,
+        padding:10,
+        borderRadius:5,
+        flex:1 //mesela yukarıdaki mainlerde flex yazmadık geldik en sonki elementin main cssine yazdık. Üstlerde flex olmayacak ya bundan dolayı kalan boşluğu da buradaki flex kapatacak. Boş alana flex atayınca orayı da flex kapatıyor 
     }
 })
