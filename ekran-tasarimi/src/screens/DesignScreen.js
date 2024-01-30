d0c137500569d8956943a10e152f6a15a677853d
Recep <recepcengiz_96@outlook.com>
2024-01-30 09:58:34 +0300

    margin ve padding işlemleri yapıldı

--- ekran-tasarimi/src/screens/DesignScreen.js
+++ ekran-tasarimi/src/screens/DesignScreen.js
@@ -5,8 +5,26 @@ export default function DesignScreen() {
   return (
     <View>
        <View style={{width:50,height:50,backgroundColor:"red"}} />
+
+        <Text style={styles.mainText}> Layout </Text>
+
+
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    mainView:{
+        borderWidth:1,
+        borderColor:"black",
+        margin:20,
+        borderRadius:10
+    },
+    mainText:{
+        borderWidth:1,
+        borderColor:"red",
+        margin:30,
+        padding:10,
+        borderRadius:5
+    }
+})
