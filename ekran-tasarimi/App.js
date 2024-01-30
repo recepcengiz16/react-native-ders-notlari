78fe650da7c3f5801f516077f33f23509b57d90e
Recep <recepcengiz_96@outlook.com>
2024-01-30 09:38:25 +0300

    navigation ayarlamaları yapıldı

--- ekran-tasarimi/App.js
+++ ekran-tasarimi/App.js
@@ -1,6 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native';
 import HomeScreen from './src/screens/HomeScreen';
 import DesignScreen from './src/screens/DesignScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
