569cc83a8b0684ceb51a510534462e3cbfd32bee
Recep <recepcengiz_96@outlook.com>
2024-01-31 10:57:57 +0300

    Ucuz, uygun ve pahalı restoranlar için componente propslar geçildi

--- uygulamalar/restaurant-app/screens/SearchScreen.js
+++ uygulamalar/restaurant-app/screens/SearchScreen.js
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import SearchBar from '../components/SearchBar'
 import useResults from '../hooks/useResults'
+import ResultList from '../components/ResultList';
 
 export default function SearchScreen() {
 
@@ -9,9 +10,19 @@ export default function SearchScreen() {
 
   console.log(results);
 
+  const filterResultsByPrice = (price)=>{
+    return results.filter((result)=>{
+      return result.price === price;
+    })
+  }
+
+
   return (
     <View>
       <SearchBar />
+      <ResultList title="Ucuz Restoranlar" result={filterResultsByPrice("₺")} />
+      <ResultList title="Uygun Restoranlar" result={filterResultsByPrice("₺₺")} />
+      <ResultList title="Pahalı Restoranlar" result={filterResultsByPrice("₺₺₺")} />
     </View>
   )
 }
