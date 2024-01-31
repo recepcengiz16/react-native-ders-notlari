5e65c825331dff85ce8197d9411f9e18a1fd5ca2
Recep <recepcengiz_96@outlook.com>
2024-01-31 13:07:24 +0300

    item içerisindeki detayları yazdım

--- uygulamalar/restaurant-app/components/ResultDetail.js
+++ uygulamalar/restaurant-app/components/ResultDetail.js
@@ -1,10 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
 
-export default function ResultDetail() {
+export default function ResultDetail({result}) {
   return (
     <View>
-      <Text>ResultDetail</Text>
+       <Image source={result.image_url ? {uri:result.image_url} : null } style={{width:250,height:120}} />
+       <Text>{result.name}</Text>
+       <Text>{result.rating} Yıldızlı Restoran, {result.review_count} Değerlendirme </Text>
     </View>
   )
 }
