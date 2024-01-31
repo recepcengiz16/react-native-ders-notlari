a0ad26fd395fa27fd1df710d8812f59cb0b05d5f
Recep <recepcengiz_96@outlook.com>
2024-01-31 08:40:57 +0300

    axios ayarlamaları yapıldı

---
+++ uygulamalar/restaurant-app/api/yelp.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const appAxios = axios.create({
+    baseURL:"https://api.yelp.com/v3/businesses",
+    headers:{
+        Authorization:"Bearer lPvHLpAP84Sp-rmvjn4w4qigOjXF3ZseZrotba-ckv6Ae9XAOLoECJHgYYs6c7XaX0FInTZohj_6uJR_SQJFWHqziUQE6SuKzo0NwKqrEMP_b4SbYcR1ygRJJM64ZXYx"
+    }
+});
+
+export default axios;
