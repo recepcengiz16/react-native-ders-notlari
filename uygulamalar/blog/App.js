48cd6f738291e2efe5eaf1682fecf1cff1d6ff29
Recep <recepcengiz_96@outlook.com>
2024-02-01 17:47:58 +0300

    Her sayfada gözükmesi için headertitle düzenlendi

--- uygulamalar/blog/App.js
+++ uygulamalar/blog/App.js
@@ -8,9 +8,9 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />    
+      <Stack.Navigator screenOptions={{ headerTitle : `Foster'ın Hayali Dostlar Mekanı` }}>
         <Stack.Screen name="Create" component={CreateScreen} />    
+        <Stack.Screen name="Home" component={HomeScreen} />    
       </Stack.Navigator>
     </NavigationContainer>
   );
