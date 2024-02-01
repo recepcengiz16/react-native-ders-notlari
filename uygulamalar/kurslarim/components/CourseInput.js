dc851f77bab503a895073faa6aa3579cefe0b677
Recep <recepcengiz_96@outlook.com>
2024-02-01 08:52:34 +0300

    Modalın içi düzenlenmeye devam ediyor

--- uygulamalar/kurslarim/components/CourseInput.js
+++ uygulamalar/kurslarim/components/CourseInput.js
@@ -1,11 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,Modal,Image } from 'react-native'
 import React from 'react'
 
 export default function CourseInput({visible}) {
   return (
-    <View>
-      <Text>CourseInput</Text>
-    </View>
+    <Modal
+        animationType="slide"       
+        visible={visible}
+      >
+        <View style={styles.centeredView}>
+            <Image />
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Hello World!</Text>
+           
+          </View>
+        </View>
+      </Modal>
   )
 }
 
