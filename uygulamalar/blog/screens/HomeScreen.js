9a0fef458cc2ac7afda22e61bcce06e720d4fe84
Recep <recepcengiz_96@outlook.com>
2024-02-02 16:12:29 +0300

    context düzenlemesi yapıldı

--- uygulamalar/blog/screens/HomeScreen.js
+++ uygulamalar/blog/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View,FlatList } from 'react-native'
+import { StyleSheet, Text, View,FlatList,Button } from 'react-native'
 import React,{useContext} from 'react'
 import BlogContext from '../context/BlogContext'
 
@@ -6,11 +6,10 @@ export default function HomeScreen() {
 
   const {data,addBlogPost}=useContext(BlogContext); //value değerini blogcontexdeki gönderdiğim value değerinden alıyorum useContextt içerisinde
 
-  
-
   return (
     <View>
       <Text>HomeScreen </Text>
+      <Button title='Ekle' onPress={addBlogPost}/>
       <FlatList
       data={data}
       renderItem={({item})=>{
