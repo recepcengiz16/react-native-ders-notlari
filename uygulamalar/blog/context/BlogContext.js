a870319f2b4391a8944f30ca9c9e1a3f180fff15
Recep <recepcengiz_96@outlook.com>
2024-02-02 17:55:48 +0300

    reducer ile blog dizisine ekleme yapıldı

--- uygulamalar/blog/context/BlogContext.js
+++ uygulamalar/blog/context/BlogContext.js
@@ -1,17 +1,37 @@
-import React,{useState} from "react";
+import React,{useState,useReducer} from "react";
 
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({children})=>{
 
-    const [blogPosts, setBlogPosts] = useState([
-        {title:"React Native"},
-        {title:"Javascript"}
-    ]);
+    const blogReducer = (state,action)=>{
+        switch (action.type) {
+            case "add_blogpost":
+                
+                return [...state,{title:"Angular"}]
+        
+            default:
+                return state;
+        }
+    }
 
     const addBlogPost = ()=>{
-        setBlogPosts([...blogPosts,{title:"Vue Js"}])
+        dispatch({type:"add_blogpost"})
     }
+
+    const [blogPosts, dispatch] = useReducer(blogReducer,[
+             {title:"React Native"},
+             {title:"Javascript"}
+    ]);
+
+    // const [blogPosts, setBlogPosts] = useState([
+    //     {title:"React Native"},
+    //     {title:"Javascript"}
+    // ]);
+
+    // const addBlogPost = ()=>{
+    //     setBlogPosts([...blogPosts,{title:"Vue Js"}])
+    // }
    // addBlogPost:addBlogPost propertynin değeri de aynı isimle olunca tek yazabiliyorduk hatırlarsan. addBlogPost callback fonks yani bu diziye ekleme yapacağım fonks.
 
    //value kısmında da dışarıda kullanacağım veriler oluyor ya da dışarıdan alacağım dinamik veriler
