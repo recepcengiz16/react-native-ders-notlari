8298224f733f21a339c3fa1cadb4b631a7631667
Recep <recepcengiz_96@outlook.com>
2024-02-02 15:31:32 +0300

    bloglistesi e

--- uygulamalar/blog/context/BlogContext.js
+++ uygulamalar/blog/context/BlogContext.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React,{useState} from "react";
 
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({children})=>{
 
-    const blogPosts=[{title:"React Native"},{title:"Javascript"}]
+    const [blogPosts, setBlogPosts] = useState([
+        {title:"React Native"},
+        {title:"Javascript"}
+    ]);
 
-    return <BlogContext.Provider value={blogPosts}>
+    const addBlogPost = ()=>{
+        setBlogPosts([...blogPosts,{title:"Vue Js"}])
+    }
+   // addBlogPost:addBlogPost propertynin değeri de aynı isimle olunca tek yazabiliyorduk hatırlarsan. addBlogPost callback fonks yani bu diziye ekleme yapacağım fonks.
+
+   //value kısmında da dışarıda kullanacağım veriler oluyor ya da dışarıdan alacağım dinamik veriler
+    return <BlogContext.Provider value={{data:blogPosts,addBlogPost}}> 
         {children}
     </BlogContext.Provider>
 }
--- uygulamalar/blog/screens/HomeScreen.js
+++ uygulamalar/blog/screens/HomeScreen.js
@@ -1,14 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,FlatList } from 'react-native'
 import React,{useContext} from 'react'
 import BlogContext from '../context/BlogContext'
 
 export default function HomeScreen() {
 
-  const value=useContext(BlogContext); //value değerini blogcontexdeki gönderdiğim value değerinden alıyorum useContextt içerisinde
+  const {data,addBlogPost}=useContext(BlogContext); //value değerini blogcontexdeki gönderdiğim value değerinden alıyorum useContextt içerisinde
+
+  
 
   return (
     <View>
-      <Text>HomeScreen {value}</Text>
+      <Text>HomeScreen </Text>
+      <FlatList
+      data={data}
+      renderItem={({item})=>{
+        return (
+          <Text>{item.title}</Text>
+        )
+      }}
+      keyExtractor={item=>item.title}
+      />
     </View>
   )
 }
