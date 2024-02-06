6506e610553e209a1b7b0f99e7de4bec6fbd87bb
Recep <recepcengiz_96@outlook.com>
2024-02-06 15:08:02 +0300

    css düzenlemeleri yapıldı

--- uygulamalar/blog/components/BlogPostForm.js
+++ uygulamalar/blog/components/BlogPostForm.js
@@ -1,12 +1,65 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native'
+import React,{useState} from 'react'
 
 export default function BlogPostForm() {
+
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+
   return (
-    <View>
-      <Text>BlogPostForm</Text>
+    <View style={styles.main}>
+      <Text style={styles.label}>Başlığı Giriniz: </Text>
+      <TextInput 
+        style={styles.input}
+        value={title}
+        onChangeText={(title)=>setTitle(title)}
+      />
+      
+      <Text style={styles.label}>İçeriği Giriniz: </Text>
+      <TextInput
+        style={styles.input}
+        value={content}
+        onChangeText={(content)=>setContent(content)}
+      />
+
+      <TouchableOpacity style={styles.buttonMain}>
+          <View style={styles.buttonView}>
+            <Text style={styles.buttonText}>Kaydet</Text>
+          </View>
+      </TouchableOpacity>
+
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    main:{
+        marginTop:10
+    },
+    label:{
+        fontSize:15,
+        marginLeft:10
+    },
+    input:{
+        borderWidth:1,
+        margin:10,
+        borderRadius:20,
+        padding:5,
+        fontSize:15,
+        marginBottom:15
+    },
+    buttonMain:{
+        padding:30
+    },
+    buttonView:{
+        backgroundColor:"green",
+        padding:5,
+        alignItems:"center",
+        justifyContent:"center",
+        borderRadius:20
+    },
+    buttonText:{
+        color:"white",
+        fontSize:20
+    }
+})
