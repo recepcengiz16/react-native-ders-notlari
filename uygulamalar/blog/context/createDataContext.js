bd07404a1100314f2e304a578f0570e3f783983f
Recep <recepcengiz_96@outlook.com>
2024-02-06 10:22:59 +0300

    provider için düzenleme yaptım

--- uygulamalar/blog/context/createDataContext.js
+++ uygulamalar/blog/context/createDataContext.js
@@ -7,9 +7,14 @@ export default (reducer,actions,initialState)=>{
 
 
         const [state, dispatch] = useReducer(reducer,initialState);
+
+        const boundActions={};
+        for (let key in actions)
+            boundActions[key]=actions[key](dispatch);
+        }
     
        //value kısmında da dışarıda kullanacağım veriler oluyor ya da dışarıdan alacağım dinamik veriler, state:state olunca tek yazıyorduk ya
-        return <Context.Provider value={{state}}> 
+        return <Context.Provider value={{state},boundedActions}> 
             {children}
         </Context.Provider>
     }
