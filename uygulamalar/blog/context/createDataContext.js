56d3308426cc2b472ea8904a191e514fc84138bf
Recep <recepcengiz_96@outlook.com>
2024-02-06 10:29:52 +0300

    dışarı açılacak olan dispatchlere erişim sağladık

--- uygulamalar/blog/context/createDataContext.js
+++ uygulamalar/blog/context/createDataContext.js
@@ -9,12 +9,12 @@ export default (reducer,actions,initialState)=>{
         const [state, dispatch] = useReducer(reducer,initialState);
 
         const boundActions={};
-        for (let key in actions)
+        for (let key in actions){
             boundActions[key]=actions[key](dispatch);
         }
     
        //value kısmında da dışarıda kullanacağım veriler oluyor ya da dışarıdan alacağım dinamik veriler, state:state olunca tek yazıyorduk ya
-        return <Context.Provider value={{state},boundedActions}> 
+        return <Context.Provider value={{state,...boundActions}}> 
             {children}
         </Context.Provider>
     }
