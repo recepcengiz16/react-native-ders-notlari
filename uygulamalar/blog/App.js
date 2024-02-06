84306517530909f18ce4b2abc07282226f49f639
Recep <recepcengiz_96@outlook.com>
2024-02-06 10:31:18 +0300

    blogpriveder ismini güncelledik

--- uygulamalar/blog/App.js
+++ uygulamalar/blog/App.js
@@ -2,20 +2,20 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import CreateScreen from './screens/CreateScreen';
-import { BlogProvider } from './context/BlogContext';
+import { Provider } from './context/BlogContext';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return ( //blogproviderı kapsayıcı olarak yazdık çünkü data alışverişinde her sayfada kullanılabilmesi için
-    <BlogProvider> 
+  return ( //providerı kapsayıcı olarak yazdık çünkü data alışverişinde her sayfada kullanılabilmesi için
+    <Provider> 
       <NavigationContainer>
         <Stack.Navigator  screenOptions={{ headerTitle : `Foster'ın Hayali Dostlar Mekanı` }}>
           <Stack.Screen name="Index" component={HomeScreen} />    
           <Stack.Screen name="Create" component={CreateScreen} />    
         </Stack.Navigator>
       </NavigationContainer>
-    </BlogProvider>
+    </Provider>
   );
 }
 
