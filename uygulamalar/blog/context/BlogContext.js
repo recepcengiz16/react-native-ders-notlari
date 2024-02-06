110eb0034c786df2687d39385c86a62eed43ad4d
Recep <recepcengiz_96@outlook.com>
2024-02-06 10:12:43 +0300

    blogcontext i oluşturduk

--- uygulamalar/blog/context/BlogContext.js
+++ uygulamalar/blog/context/BlogContext.js
@@ -1,43 +1,22 @@
 import React,{useState,useReducer} from "react";
+import createDataContext from "./createDataContext";
 
 const BlogContext = React.createContext();
 
-export const BlogProvider = ({children})=>{
-
-    const blogReducer = (state,action)=>{
-        switch (action.type) {
-            case "add_blogpost":
-                
-                return [...state,{title:"Angular"}]
-        
-            default:
-                return state;
-        }
+const blogReducer = (state,action)=>{
+    switch (action.type) {
+        case "add_blogpost":
+            
+            return [...state,{title:"Angular"}]
+    
+        default:
+            return state;
     }
-
-    const addBlogPost = ()=>{
-        dispatch({type:"add_blogpost"})
-    }
-
-    const [blogPosts, dispatch] = useReducer(blogReducer,[
-             {title:"React Native"},
-             {title:"Javascript"}
-    ]);
-
-    // const [blogPosts, setBlogPosts] = useState([
-    //     {title:"React Native"},
-    //     {title:"Javascript"}
-    // ]);
-
-    // const addBlogPost = ()=>{
-    //     setBlogPosts([...blogPosts,{title:"Vue Js"}])
-    // }
-   // addBlogPost:addBlogPost propertynin değeri de aynı isimle olunca tek yazabiliyorduk hatırlarsan. addBlogPost callback fonks yani bu diziye ekleme yapacağım fonks.
-
-   //value kısmında da dışarıda kullanacağım veriler oluyor ya da dışarıdan alacağım dinamik veriler
-    return <BlogContext.Provider value={{data:blogPosts,addBlogPost}}> 
-        {children}
-    </BlogContext.Provider>
 }
 
-export default BlogContext;
+const addBlogPost = ()=>{
+    dispatch({type:"add_blogpost"})
+}
+
+
+export const {Context,Provider} = createDataContext(blogReducer,{addBlogPost},[]); //burada generic oluşturduğumuz contexti çağırıp contexti oluşturuyoruz.
