21e0eb0e31ff0407982aafced28c353a54535022
Recep <recepcengiz_96@outlook.com>
2024-02-06 13:49:20 +0300

    form postu componenti yerleştirildi

--- uygulamalar/blog/screens/CreateScreen.js
+++ uygulamalar/blog/screens/CreateScreen.js
@@ -1,10 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import BlogPostForm from '../components/BlogPostForm'
 
 export default function CreateScreen() {
   return (
     <View>
-      <Text>CreateScreen</Text>
+     <BlogPostForm />
     </View>
   )
 }
