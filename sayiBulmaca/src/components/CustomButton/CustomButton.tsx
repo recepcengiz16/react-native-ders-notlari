e3b46fac52e51e14be937634f9dc95851b3baf89
Recep <recepcengiz_96@outlook.com>
2024-04-01 10:22:46 +0300

    butonlara event eklendi

--- sayiBulmaca/src/components/CustomButton/CustomButton.tsx
+++ sayiBulmaca/src/components/CustomButton/CustomButton.tsx
@@ -2,11 +2,13 @@ import { Text, View, Pressable } from 'react-native'
 import React from 'react';
 import styles from "./CustomButton.style"
 
-export default function CustomButton({children}): React.JSX.Element {
+export default function CustomButton({children,onPress}): React.JSX.Element {
   return (
     <View style={styles.buttonContainer}>
       <Pressable
-        style={({pressed})=> pressed ? [styles.buttonInnerContainer,styles.pressed] : styles.buttonInnerContainer}>
+        style={({pressed})=> pressed ? [styles.buttonInnerContainer,styles.pressed] : styles.buttonInnerContainer}
+        onPress={onPress}
+      >
         <Text style={styles.btnText}>{children}</Text>
       </Pressable>
     </View>
