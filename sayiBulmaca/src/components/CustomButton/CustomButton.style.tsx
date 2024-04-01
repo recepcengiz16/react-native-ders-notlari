385f46d2b8d5c4440ff5050f99f2f35dcbc305cf
Recep <recepcengiz_96@outlook.com>
2024-04-01 10:19:38 +0300

    css düzenlemeleri yapıldı

--- sayiBulmaca/src/components/CustomButton/CustomButton.style.tsx
+++ sayiBulmaca/src/components/CustomButton/CustomButton.style.tsx
@@ -1,5 +1,18 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-
+    buttonContainer:{
+        margin:5
+    },
+    buttonInnerContainer:{
+        paddingVertical:8
+    },
+    btnText: {
+        textAlign:"center",
+        color:"white",
+        fontSize:20
+    },
+    pressed:{
+        opacity:0.5
+    }
 });
--- sayiBulmaca/src/components/CustomButton/CustomButton.tsx
+++ sayiBulmaca/src/components/CustomButton/CustomButton.tsx
@@ -1,13 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, View, Pressable } from 'react-native'
 import React from 'react';
 import styles from "./CustomButton.style"
 
 export default function CustomButton({children}): React.JSX.Element {
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.buttonContainer}>
+      <Pressable
+        style={({pressed})=> pressed ? [styles.buttonInnerContainer,styles.pressed] : styles.buttonInnerContainer}>
+        <Text style={styles.btnText}>{children}</Text>
+      </Pressable>
     </View>
   )
 }
-
-const styles = StyleSheet.create({})
