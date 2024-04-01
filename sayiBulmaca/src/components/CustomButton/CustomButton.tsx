67843c9bc964c4810e85c0865a713c814b11fd4f
Recep <recepcengiz_96@outlook.com>
2024-04-01 09:17:10 +0300

    buton kompenenti oluşturuldu

---
+++ sayiBulmaca/src/components/CustomButton/CustomButton.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import styles from "./CustomButton.style"
+
+export default function CustomButton(): React.JSX.Element {
+  return (
+    <View>
+      <Text>CustomButton</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
